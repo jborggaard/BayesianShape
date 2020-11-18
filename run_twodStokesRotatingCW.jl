cc50fbc7f5a5a4b1c44d6fd24e44f6a68d629ab8
Jeff Borggaard <jborggaa@Jeffs-Mac-Pro.local>
2020-11-18 13:00:28 -0500

    updated twodStokesRotatingCW and checked

--- run_twodStokesRotatingCW.jl
+++ run_twodStokesRotatingCW.jl
@@ -23,38 +23,40 @@ include("makeMesh.jl")
 include("fitBSpline2Fourier.jl")
 include("saveFEMasVTK.jl")
 include("twodQuadratureRule.jl")
+include("sampleInnerGeometry.jl")
 include("twodShape.jl")
 include("twodBilinear.jl")
 include("twodLinForm.jl")
 include("twodStokesRotatingCW.jl")
 include("twodAdvectionDiffusion.jl")
+include("twodProjectDerivatives.jl")
+include("computeC.jl")
+include("computeVorticity.jl")
 
-
-ω = 30.0;    # rotational velocity
+ω = 10.0;    # rotational velocity for outer wall
 
 ### define the 40 parameters that describe the inner boundary
 N = 40;
 
+a0,a,b = sampleInnerGeometry()
+
 #### First draw 40 parameters from a normal distribution
 # for sane reproducibility in debugging
-Random.seed!(1);
-param = randn(Float64,40);
-#param = ones(Float64,40);
-#param = zeros(40);
+#Random.seed!(1);
+#param = randn(Float64,N);
+#param = ones(Float64,N);
+#param = zeros(N);
 #param[1] = 0; param[2] = 1;
 
 #### Then map them to a distribution between 0.5 and 1.5 using the arctan function, small alpha values (e.g. 0.1) cluster the results of the B-spline parameters around 1
 
-α = 1.0;
-r = 1.0 .+ atan.(α*param)/π;
-#r = ones(40,1);
+#α = 1.0;
+#r = 1.0 .+ atan.(α*param)/π;
 ### Generate the finite element mesh using Gmsh (implemented in makeMesh)
 
-a0 = 1.0;
-a0 = 1; a = [-1/2 -1/8 -1/8 1/18 1/18 0.0 0.0 1/8]; b = [1/4 -1/8 0 1/4 -1/8 0.0 0.0 1/8];
 r = fitBSpline2Fourier(a0,a,b,N)
-
-x,eConn, innerNodes,innerX, outerNodes,outerX = makeMesh(r)
+#r = ones(N,1); # uncomment for some degubbing
+x,eConn,eConn2, innerNodes,innerX, outerNodes,outerX = makeMesh(r)
 
 sort!(innerNodes);
 sort!(outerNodes);
@@ -63,19 +65,38 @@ sort!(outerNodes);
 #   Let's look at the mesh...
 nNodes = size(x,2)
 nElements = size(eConn,2)
+nElementsSensor = size(eConn2,2)
 
-xT = zeros(Float64,nNodes,2)
-eC = zeros(Int64,nElements,6)
+xT = zeros(Float64,nNodes,2)   # xT = transpose(x[1:2,:])
+eC = zeros(Int64,nElements,6)  # eC = transpose(eConn), converted to Int64
 for i=1:nNodes
   xT[i,1] = x[1,i]
   xT[i,2] = x[2,i]
 end
-for i=1:nElements
-  for j=1:6
-    eC[i,j] = convert(Int64,eConn[j,i])
-  end
+for i=1:nElements#-nElementsSensor # the sensor elements are orientated correctly (why?)
+  eC[i,1] = convert(Int64,eConn[1,i])
+  eC[i,2] = convert(Int64,eConn[3,i])
+  eC[i,3] = convert(Int64,eConn[2,i])
+  eC[i,4] = convert(Int64,eConn[6,i])
+  eC[i,5] = convert(Int64,eConn[5,i])
+  eC[i,6] = convert(Int64,eConn[4,i])
 end
 
+eC2  = zeros(Int64,nElementsSensor,6)
+for i=1:nElementsSensor
+  for j=1:6
+    eC2[i,j] = convert(Int64,eConn2[j,i])
+  end
+end  # elements are positively orientated
+C    = computeC(xT,eC2)
+
+Call = computeC(xT,eC)
+
+#for i=nElements-nElementsSensor+1 : nElements
+#  for j=1:6
+#    eC[i,j] = convert(Int64,eConn[j,i])
+#  end
+#end
 #Plots.plot([x[1,innerNodes],x[1,outerNodes]],[x[2,innerNodes],x[2,outerNodes]],seriestype = :scatter)
 
 
@@ -83,11 +104,31 @@ velocity, pressure = twodStokesRotatingCW(xT,eC,innerNodes,outerNodes,ω)
 
 temperature = twodAdvectionDiffusion(xT,eC,innerNodes,outerNodes,velocity)
 
-#scalarLabels = ["temperature"]
-#vectorLabels = ["velocity"]
-#saveFEMasVTK("mixing",xT,eC,scalarLabels,temperature,vectorLabels,velocity)
+#  Output the solution or visualize
+scalarLabels = ["temperature"]
+vectorLabels = ["velocity"]
+saveFEMasVTK("mixingCW",xT,eC,scalarLabels,temperature,vectorLabels,velocity)
+
+#poly(xT, eC[:,1:3], color = velocity[:,2], strokecolor = (:black, 0.6), strokewidth = .2)
+
+vorticity = computeVorticity(xT,eC,velocity)
+#saveFEMasVTK("mixingCW",xT,eC,scalarLabels,vorticity,vectorLabels,velocity)
+poly(xT, eC[:,1:3], color = vorticity[:,1], strokecolor = (:black, 0.6), strokewidth = 0.2)
 
 velMag = sqrt.( velocity[:,1].*velocity[:,1] + velocity[:,2].*velocity[:,2] )
-#poly(xT, eC[:,1:3], color = velocity[:,1], strokecolor = (:black, 0.6), strokewidth = .2)
-poly(xT, eC[:,1:3], color = velMag, strokecolor = (:black, 0.6), strokewidth = .3)
+#poly(xT, eC[:,1:3], color = velMag, strokecolor = (:black, 0.6), strokewidth = .3)
+#poly(xT, eC[:,1:3], color = temperature[:,1], strokecolor = (:black, 0.6), strokewidth = .2)
 poly(xT, eC[:,1:3], color = temperature[:,1], strokecolor = (:black, 0.6), strokewidth = .2)
+
+V2 = sum(C)
+Volume = sum(Call)
+
+res = (C*temperature)/V2
+@printf("the average temperature over the subregion is %g\n",res[1])
+res = (Call*temperature)/Volume
+@printf("                    and over the entire region is %g\n\n",res[1])
+
+res = (C*vorticity)/V2
+@printf("the average vorticity over the subregion is %g\n",res[1])
+res = (Call*vorticity)/Volume
+@printf("                  and over the entire region is %g\n\n\n",res[1])
