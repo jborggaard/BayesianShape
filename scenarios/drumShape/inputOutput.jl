33187aa96553ba11f7e975820b9e479b48cdebd3
Justin Krometis <jkrometis@vt.edu>
2021-07-22 09:31:33 -0400

    inputOutput.jl: add atan() smoothing + optional arguments

--- scenarios/drumShape/inputOutput.jl
+++ scenarios/drumShape/inputOutput.jl
@@ -1,25 +1,25 @@
-function inputOutput(a0,a,b)
-
-  N = 40  # number of BSplines used to represent the drum shape
-
+# N:   number of BSplines used to represent the drum shape
+# nev: how many frequencies we hear
+function inputOutput(a0,a,b; N=40, nev = 1, κ=1.0, rMax=1.25, rMin=0.75)
 
   r,err = fitBSpline2Fourier(a0,a,b,N)
   #@printf("B-spline approximation error (%d B-splines) is: %12.8f\n",N,err)
+  
+  ### Then map them to a distribution between rMin and rMax using the arctan function, small alpha values (e.g. 0.1) cluster the results of the B-spline parameters around 1
+  α = 1.0;
+  r = 0.5*(rMax+rMin) .+ (rMax-rMin)*atan.(α*r)/π;
+  println("Extrema of r are:");
+  display(extrema(r));
 
   x,eConn,boundaryNodes = makeDrumMesh(r)
 
   nBoundary = length(boundaryNodes)
   dBoundary = zeros(Float64,nBoundary,1)
-  A,M = computeFEMmatrices(x,eConn,boundaryNodes,dBoundary)
+  A,M = computeFEMmatrices(x,eConn,boundaryNodes,dBoundary;κ=κ)
 
-  #  how many frequencies do we hear?
-  nev = 1
   λ, ϕ = eigs(A,M; tol=1e-6, which=:SM, nev)
 
-  realλ = zeros(Float64,nev)
-  for i=1:nev
-    realλ[i] = real(λ[i])
-  end
+  realλ = real(λ)
 
   return realλ
 
